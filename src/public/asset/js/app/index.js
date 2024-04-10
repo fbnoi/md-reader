815eb20279a0f971b45a85ee9f8ca83d8cb6e22d
Lang Liu <lang.liu@mdpi.com>
2024-04-10 21:57:44 +0800

    Update home page

--- src/public/asset/js/app/index.js
+++ src/public/asset/js/app/index.js
@@ -3,19 +3,21 @@ document.addEventListener('DOMContentLoaded', function () {
         resolve(window.API.getHistory());
     }).then((history) => {
         if (typeof history.dir !== "undefined") {
-            let html = '';
+            let html = '<ul>';
             history.dir.forEach(dir => {
-                html += `<div class="dir-history-item" property-path="${dir.path}">${dir.name}</div>`;
+                html += `<li class="dir-history-item" property-path="${dir.path}">${dir.name}</li>`;
             });
+            html += '</ul>';
             document.querySelector('.dir-history').innerHTML = html;
         }
         return history;
     }).then((history) => {
         if (typeof history.file !== "undefined") {
-            let html = '';
+            let html = '<ul>';
             history.file.forEach(file => {
-                html += `<div class="file-history-item" property-path="${file.path}">${file.name}</div>`;
+                html += `<li class="file-history-item" property-path="${file.path}">${file.name}</li>`;
             });
+            html += '</ul>';
             document.querySelector('.file-history').innerHTML = html;
         }
         return history;
