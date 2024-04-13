cca3b0cb42a0c25f8542eb5fef4a398eccf6ec4a
浪 <fbnoi@foxmail.com>
2024-04-13 20:53:43 +0800

    Update

--- src/public/asset/js/app/tool.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    document.querySelector('.tool-open-index').addEventListener('click', window.API.openIndexPage);
-    document.querySelector('.tool-open-dir').addEventListener('click', window.API.openDirDialog);
-    document.querySelector('.tool-open-file').addEventListener('click', window.API.openFileDialog);
-    document.querySelector('.tool-dev-debug').addEventListener('click', window.API.openDevDebug);
-});
