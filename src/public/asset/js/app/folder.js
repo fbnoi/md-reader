806819c18d9ce68dafef25f9cb9ebfecacc66ee4
浪 <fbnoi@foxmail.com>
2024-04-14 15:34:52 +0800

    Fix code

--- src/public/asset/js/app/folder.js
+++ src/public/asset/js/app/folder.js
@@ -8,11 +8,31 @@ document.addEventListener('DOMContentLoaded', function () {
     function splitResize() {
         sideViewElem.style['grid-template-rows'] = '1fr 5px ' + sideViewElem.clientHeight * radio + 'px';
     }
-
     function updateRadio() {
         radio = categoryElem.clientHeight / sideViewElem.clientHeight;
     }
-    
+    function openFile(filepath) {
+        new Promise(resolve => {
+            resolve(window.API.openFile(filepath));
+        }).then((fileInfo) => {
+            document.title = fileInfo.name;
+            articleElem.innerHTML = fileInfo.doc.html;
+            articleElem.scrollTop = 0;;
+            categoryElem.innerHTML = fileInfo.doc.toc;
+            if (!initialed) {
+                initialed = true;
+                radio = 0.5;
+                splitResize();
+            }
+        }).then(() => {
+            document.querySelectorAll('a.open-in-browser').forEach((link) => {
+                link.addEventListener('click', (e) => {  
+                    e.preventDefault();
+                    window.API.openExternal(link.href);
+                }); 
+            });
+        });
+    }
     new Promise((resolve) => {
         Split({
             columnGutters: [{
@@ -37,39 +57,15 @@ document.addEventListener('DOMContentLoaded', function () {
     }).then((nodes) => {
         Dree(nodes, {
             element: document.querySelector('.file-tree'),
-            onSeletct: (node) => console.log(node),
+            onSelect: (node) => {
+                if (node.type != 1) {
+                    openFile(node.path);
+                }
+            },
             onExpand: (node) => console.log(node),
             onFold: (node) => console.log(node),
         });
-        // document.querySelector('.file-tree').innerHTML = html;
         return;
-    }).then(() => {
-
-        // document.querySelectorAll('.file-tree a[property-type="2"]').forEach(elem => {
-        //     elem.addEventListener('click', function() {
-        //         const filePath = this.getAttribute('property-path');
-        //         new Promise(resolve => {
-        //             resolve(window.API.openFile(filePath));
-        //         }).then((fileInfo) => {
-        //             document.title = fileInfo.name;
-        //             articleElem.innerHTML = fileInfo.doc.html;
-        //             articleElem.scrollTop = 0;;
-        //             categoryElem.innerHTML = fileInfo.doc.toc;
-        //             if (!initialed) {
-        //                 initialed = true;
-        //                 radio = 0.5;
-        //                 splitResize();
-        //             }
-        //         }).then(() => {
-        //             document.querySelectorAll('a.open-in-browser').forEach((link) => {
-        //                 link.addEventListener('click', (e) => {  
-        //                     e.preventDefault();
-        //                     window.API.openExternal(link.href);
-        //                 }); 
-        //             });
-        //         });
-        //     })
-        // });
     });
 
     window.addEventListener('resize', splitResize);
