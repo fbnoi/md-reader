f7c661f952b503b7813f4887d196179ea6f94217
Lang Liu <lang.liu@mdpi.com>
2024-05-08 17:24:43 +0800

    Stash

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,20 @@
 const path = require("path");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
     mode: "development", // 或 'production'
     entry: {
+        screen: "./src/js/screen.js",
         home: "./src/js/home.js",
-        file: "./src/js/file.js",
-        folder: "./src/js/folder.js"
+        // file: "./src/js/file.js",
+        // folder: "./src/js/folder.js"
     },
+    // devtool: false,
     output: {  
         path: path.resolve(__dirname, 'dist'),  
-        filename: '[name].bundle.js' // 使用 [name] 占位符来根据入口点名称生成不同的 bundle 文件名  
+        filename: 'js/[name].bundle.js' // 使用 [name] 占位符来根据入口点名称生成不同的 bundle 文件名
     },
     module: {
         rules: [
@@ -37,9 +40,24 @@ module.exports = {
         ],
     },
     plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/html/home.html',
+            filename: 'home.html',
+            chunks: ['home', 'screen']
+        }),
+        // new HtmlWebpackPlugin({
+        //     template: './src/html/file.html',
+        //     filename: 'file.html',
+        //     chunks: ['file', 'screen']
+        // }),
+        // new HtmlWebpackPlugin({
+        //     template: './src/html/folder.html',
+        //     filename: 'folder.html',
+        //     chunks: ['folder', 'screen']
+        // }),
         new CleanWebpackPlugin(), // 清理输出目录
         new MiniCssExtractPlugin({
-            filename: "[name].[contenthash].css", // 提取 CSS 到单独的文件
+            filename: "css/[name].[contenthash].css", // 提取 CSS 到单独的文件
         }),
         // ... 其他插件配置
     ],
